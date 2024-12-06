e1fa1d3432f56aafcc1010b085e47c5dc2a326a1
Entropy675 <sibteali@outlook.com>
2024-12-06 17:16:57 -0500

    removed physics logic from player/camera classes

--- src/core/Camera.cpp
+++ src/core/Camera.cpp
@@ -16,16 +16,24 @@ void Camera::_update()
 
 void Camera::_input()
 {
-    float moveSpeed = 10; // some default move to header
-    if (playersInScene.size()) moveSpeed = playersInScene[currPlayer]->getPlayerSpeed();
-    if (inputManager->getHeld('w')) camera.dolly(-moveSpeed); // Move forward
-    if (inputManager->getHeld('s')) camera.dolly(moveSpeed); // Move backward
-    if (inputManager->getHeld('a')) camera.truck(-moveSpeed); // Move left
-    if (inputManager->getHeld('d')) camera.truck(moveSpeed); // Move right'
+    float moveSpeed = 10; // some default value move to header
+    bool defaultControls = true;
+    if (playersInScene.size())
+    {
+        moveSpeed = playersInScene[currPlayer]->getPlayerSpeed();
+        defaultControls = playersInScene[currPlayer]->defaultControlsEnabled();
+    }
 
-    if (inputManager->getHeld(' ')) camera.move(0, moveSpeed, 0); // Move up (Space key)
-    if (inputManager->getHeld(OF_KEY_SHIFT)) camera.boom(-moveSpeed); // Move down (Shift key)
+    if (defaultControls)
+    {
+        if (inputManager->getHeld('w')) camera.dolly(-moveSpeed); // Move forward
+        if (inputManager->getHeld('s')) camera.dolly(moveSpeed); // Move backward
+        if (inputManager->getHeld('a')) camera.truck(-moveSpeed); // Move left
+        if (inputManager->getHeld('d')) camera.truck(moveSpeed); // Move right'
 
+        if (inputManager->getHeld(' ')) camera.move(0, moveSpeed, 0); // Move up (Space key)
+        if (inputManager->getHeld(OF_KEY_SHIFT)) camera.boom(-moveSpeed); // Move down (Shift key)
+    }
     if (!playersInScene.size()) return; // the rest of the func is if we have a player/players
 
     //switch Players
--- src/core/Player.h
+++ src/core/Player.h
@@ -6,14 +6,14 @@
 class Player : public PhysicsEntity
 {
 protected:
-    glm::vec3 position;
     std::string name;
     float health;
     float moveSpeed;
+    bool defaultControls = true;
 
 public:
     Player(std::string name, glm::vec3 pos = glm::vec3(0, 0, 0), float health = 100.0f, float moveSpeed = 50.0f)
-        : PhysicsEntity(pos), position(pos), name(name), health(health), moveSpeed(moveSpeed) { addTag("player"); };
+        : PhysicsEntity(pos), name(name), health(health), moveSpeed(moveSpeed) { addTag("player"); };
 
     virtual Entity* clone() const = 0;
     virtual void _collision(PhysicsEntity& target) = 0;
@@ -21,9 +21,8 @@ public:
     virtual void _update() = 0;
     virtual void _draw() = 0;
 
-    void setPosition(glm::vec3 newPos)                  { position = newPos; };
-    const glm::vec3& getPosition()                      { return position; };
-
+    void toggleDefaultControls()                        { defaultControls = !defaultControls; };
+    bool defaultControlsEnabled() const                 { return defaultControls; };
     std::string getPlayerName() const                   { return name; };
     float getPlayerSpeed() const                        { return moveSpeed; };
 };
